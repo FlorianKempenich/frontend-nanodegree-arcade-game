06c906aad1b1a4b9f35ec283e77b1fc8fcef96db
Florian Kempenich <Flori@nKempenich.com>
2019-08-01 11:51:37 +0100

    Implement basic enemy movement & remove comments

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
-// Enemies our player must avoid
-
+function resetXPosition() {
+  const offScreenRow = -Constants.colWidth;
+  this.x = offScreenRow;
+}
 
 class Enemy {
-  constructor(row, speed) {
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+  constructor(row, speed, debugName = "") {
     this.sprite = "images/enemy-bug.png";
+    this.speed = speed;
+    this.y = row * Constants.rowHeight - 20; // 20 offset is due to shape of png img for the enemy-bug
+    this.x = undefined;
+    this.debugName = debugName;
+
+    resetXPosition.call(this);
   }
 
-  // Update the enemy's position, required method for game
-  // Parameter: dt, a time delta between ticks
   update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += 100 * dt * this.speed;
+    if (this.x >= Constants.width && this.debugName !== "") {
+      resetXPosition.call(this);
+    }
   }
 
-  // Draw the enemy on the screen, required method for game
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
@@ -29,13 +33,11 @@ class Player {
   render() {}
 }
 
-// Now instantiate your objects.
-
 const allEnemies = [
-  new Enemy(1, 1),
+  new Enemy(1, 1, "other"),
   new Enemy(2, 1.3),
   new Enemy(2, 2),
-  new Enemy(3, 0.4),
+  new Enemy(3, 0.4, "frank"),
   new Enemy(3, 1.6)
 ];
 const player = new Player();
