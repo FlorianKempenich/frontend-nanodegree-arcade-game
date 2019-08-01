320e50acf2c72960066cde1f12deacb74f983279
Florian Kempenich <Flori@nKempenich.com>
2019-08-01 11:47:51 +0100

    Refactor to ES6 classes

--- js/app.js
+++ js/app.js
@@ -1,33 +1,44 @@
 // Enemies our player must avoid
-var Enemy = function() {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
 
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = "images/enemy-bug.png";
-};
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-};
+class Enemy {
+  constructor(row, speed) {
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = "images/enemy-bug.png";
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+  }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+  // Draw the enemy on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
+
+class Player {
+  constructor() {}
+  handleInput(direction) {}
+  update() {}
+  render() {}
+}
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
+const allEnemies = [
+  new Enemy(1, 1),
+  new Enemy(2, 1.3),
+  new Enemy(2, 2),
+  new Enemy(3, 0.4),
+  new Enemy(3, 1.6)
+];
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
